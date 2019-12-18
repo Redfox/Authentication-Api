b1cac9be807d2418cddc4ecb89f118d54ce5427d
vitor.pereira <vitor.pereira@giganti.com.br>
2019-12-18 11:47:28 -0300

    feat: middleware de autenticacao JWT

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+import jwtConfig from '../../config/jwt';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ mensagem: 'Token não informado' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = promisify(jwt.verify)(token, jwtConfig.secret);
+
+    req.userIs = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ mensagem: 'Sessão inválid' });
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,14 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import AuthController from './app/controllers/AuthController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/signup', UserController.store);
-
 routes.post('/signin', AuthController.store);
+
+routes.use(authMiddleware);
 
 routes.get('/', (req, res) => {
   res.json({ mensagem: 'hello world' });

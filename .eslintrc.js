29df3080ec5ffabaf2a2481fb57bf415cd0a7f2b
VitorRedfox <vitor_pereira.2013@hotmail.com>
2019-12-17 21:52:39 -0300

    chore: configurado jest para tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,8 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['airbnb-base', 'prettier'],
-  plugins: ['prettier'],
+  extends: ['airbnb-base', 'prettier', 'plugin:jest/recommended'],
+  plugins: ['prettier', 'jest'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -14,6 +14,11 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error",
     "prettier/prettier": "error",
     "class-methods-use-this": "off",
     "no-param-reassign": "off",

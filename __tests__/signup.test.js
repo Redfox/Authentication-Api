55de532a38aa7f7b58770d697e4855e692cf90db
VitorRedfox <vitor_pereira.2013@hotmail.com>
2019-12-17 23:28:03 -0300

    test: cadastro de usuario padrao

--- __tests__/signup.test.js
+++ __tests__/signup.test.js
@@ -1,26 +1,46 @@
 import request from 'supertest';
 import app from '../src/app';
 
-describe('Cadastro de um usuario', () => {
+import user from '../src/app/models/User';
+
+const body = {
+  nome: 'vitor',
+  email: 'vitor@gmail.com',
+  senha: '123',
+  telefones: [
+    {
+      ddd: 11,
+      numero: 961878675,
+    },
+    {
+      ddd: 11,
+      numero: 964579563,
+    },
+  ],
+};
+
+describe('Cadastro de usuario com sucesso', () => {
+  beforeEach(async () => {
+    if (await user.countDocuments()) {
+      user.collection.drop();
+    }
+  });
+
   it('Deve cadastrar o usuario e retornar o token de autenticacao', async () => {
     const response = await request(app)
       .post('/signup')
-      .send({
-        nome: 'vitor',
-        email: 'vitor@gmail.com',
-        senha: '123',
-        telefones: [
-          {
-            ddd: 11,
-            numero: 961878675,
-          },
-          {
-            ddd: 11,
-            numero: 964579563,
-          },
-        ],
-      });
+      .send(body)
+      .expect(200);
 
     expect(response.body).toHaveProperty('token');
+  });
+
+  it('Deve cadatrar um usuario', async () => {
+    const response = await request(app)
+      .post('/signup')
+      .send(body)
+      .expect(200);
+
+    expect(response.body).toHaveProperty('id');
   });
 });
